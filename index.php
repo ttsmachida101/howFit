0dd742e979e2233630aa48b4cc3c0c4c831a6abb
ttsmachida101 <tinotendamachida.student@gmail.com>
2020-06-09 03:28:34 -0500

    Structuring and CSS

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
-
-<html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>How Fit</title>
-
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-        <link rel=stylesheet href="./assets/css/all.min.css">
-        <link rel="stylesheet" href="./assets/css/style.css">
-    </head>
-    
-    
-   
-    <?Php
+  <?Php
 echo ("Server Running");
 
 if (isset ($_POST['submit']))
@@ -25,9 +7,17 @@ if (isset ($_POST['submit']))
 }
 ?>
 
- <body>
 <html>
+    <head>
+        <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>How Fit Are You</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link rel=stylesheet href="./assets/css/all.min.css">
+        <link rel="stylesheet" href="./assets/css/style.css">
+    </head>
 
+ <body>
 <!--------------Header---------------->
 <section class = "header">
  <div class = "container-fluid">
@@ -40,26 +30,39 @@ if (isset ($_POST['submit']))
 </div>
 </section>
 
- <div class = "container-fluid">
- <div class = "container">
- <div class="mx-auto" style="width: 600px;">
+<!--------Data Collection Container-------->
+ <section class = "data collection"> 
+ <div class = "container" id = "dataCollection">
+
+ <div class = "workoutimage">
+
+
  
    
-   <div class="form-group row">
-       <form action = "calculation.php" method = "POST">
-       <h3> Enter Your Details </h3>
-       <div>   
+ <div class="form-group row">
+<form action = "calculation.php" method = "POST">
+<h3 class="text-center"> Enter Your Details </h3>
+<div>   
     
          <!----GENDER---->
 <br>
-<b>Please enter your gender</b></p>
-<input type="radio" id="male" name="gender" value="male" required>
-<label for="male">Male</label><br>
-<input type="radio" id="female" name="gender" value="female">
-<label for="female">Female</label><br>
-<input type="radio" id="other" name="gender" value="other">
-<label for="other">Other</label></div>
-<div>
+<h5 class="text-center"> Please enter your gender</h5>
+
+<div class="form-check form-check-inline"> 
+
+<label for="male">Male</label>
+<input class="form-check-input" type="radio" id="male" name="gender" value="male" required>
+
+
+
+<label for="female">Female</label>
+<input class="form-check-input" type="radio" id="female" name="gender" value="female">
+
+<label for="other">Other</label>
+<input class="form-check-input" type="radio" id="other" name="gender" value="other">
+
+</div>
+
 <br>
 
   <!----Measurements---->
@@ -74,27 +77,33 @@ if (isset ($_POST['submit']))
     <input type = "number" class="form-control" name = "weight" required><br><br>
     
 </div>
-<div>
+
 
   <!---- Workout Activity---->
+ <div class="form-check form-check-inline">
 <p><b>How many times per week do you work-out?</b></p>
-<input type="radio" id="zero" name="workout" value="zero" required>
+<input class="form-check-input" type="radio" id="zero" name="workout" value="zero" required>
 <label for="zero">0 -1 times per week</label><br>
-<input type="radio" id="two to three times" name="workout" value="two to three times">
+<input class="form-check-input" type="radio" id="two to three times" name="workout" value="two to three times">
 <label for="two to three times"> 2- 3 times per week</label><br>
-<input type="radio" id="more than three times" name="workout" value="more than three times">
+<input class="form-check-input" type="radio"id="more than three times" name="workout" value="more than three times">
 <label for="more than three times"> More than 3 times per week</label>
 </div>
+
+
   <!----Daily Activity---->
 <div>
+    <div class="form-check form-check-inline">
 <p><b>How active are you on a daily baisis</b></p>
-<input type="radio" id="Not active" name="activity" value="Not Active" required>
+<input class="form-check-input" type="radio" id="Not active" name="activity" value="Not Active" required>
 <label for="Not active">Not very active(Office Job - I spend a lot of time sitting)</label><br>
-<input type="radio" id="moderately active" name="activity" value="moderately active">
+<input class="form-check-input" type="radio"id="moderately active" name="activity" value="moderately active">
 <label for="moderately active">Moderately active(I usually take long walks)</label><br>
-<input type="radio" id="very active" name="activity" value="very active">
+<input class="form-check-input" type="radio" id="very active" name="activity" value="very active">
 <label for="very active">Very Active (I Spend the day on my feet)</label>
 </div>
+</div>
+
 </div>
 
 <br><br>
@@ -103,11 +112,11 @@ if (isset ($_POST['submit']))
   </div>       
 </form> 
  </div>
+   </div>
   </div>
-  </div>
-  </div>
-  </div>
-<script src="" async defer></script>
+  </section>
+  
+  <!---------Scripts--------->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
